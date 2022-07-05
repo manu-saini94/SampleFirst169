d9b7dc49b63f4b5784ebb9e7e19bee0f0cc62e1e
manu-saini94 <manu.saini94@gmail.com>
2022-07-05 12:51:13 +0530

    branch-4 dev-1 commit

--- src/com/bridgelabz/basics/Sample.java
+++ src/com/bridgelabz/basics/Sample.java
@@ -4,6 +4,6 @@ public class Sample {
 
     public static void main(String[] args) {
         System.out.println("Sample main method updated by developer-1");
-        System.out.println("Sample main method updated by developer-2");
+        System.out.println("Sample main method updated by developer-2 again updated by dev-1");
     }
 }
