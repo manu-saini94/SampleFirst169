fe4e83bb3c54c8e8de9db02d9aa0a89d8ecf109d
manu-saini94 <manu.saini94@gmail.com>
2022-07-05 12:33:27 +0530

    dev-1 sample.java updated

--- src/com/bridgelabz/basics/Sample.java
+++ src/com/bridgelabz/basics/Sample.java
@@ -3,6 +3,6 @@ package com.bridgelabz.basics;
 public class Sample {
 
     public static void main(String[] args) {
-        System.out.println("Sample main method");
+        System.out.println("Sample main method updated by developer-1");
     }
 }
